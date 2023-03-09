59b64cedf2073f6bf7de18e0b696701c4d64eb98
CJ3016846 <CJ3016846@LAB01-PC17>
2023-03-09 09:54:20 -0300

    Adicionar arquivos de projeto.

---
+++ lista1PR2/Program.cs
@@ -0,0 +1,553 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1PR2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a, cent, mili;
+            //    Console.Write("digite um número de B: ");
+            //a = int.Parse(Console.ReadLine());
+            //cent = (a * 60);
+            //    mili = (a * 1000);
+
+
+            //    Console.WriteLine("os centimetros são " + cent+" e os milimetros "+mili);
+
+
+            //Console.ReadKey();
+
+            //tarefa de centimetros e milimetros
+
+            //float fahrenheit, celsius;
+
+            //Console.WriteLine("digite o número em fahrenhaeit: ");
+            //fahrenheit = float.Parse(Console.ReadLine());
+
+            //celsius = (fahrenheit - 32.0f) * (5.0f / 9.0f);
+
+            //Console.WriteLine("os graus são " + celsius);
+            //Console.ReadKey();
+            //ex 2 fahrenheit a
+
+            //    int fahrenheit, celsius;
+
+            //Console.WriteLine("digite o número em fahrenhaeit: ");
+            //fahrenheit = int.Parse(Console.ReadLine());
+
+            //celsius = (fahrenheit - 32.0f) * (5.0f / 9.0f);
+
+            //Console.WriteLine("os graus são " + celsius);
+            //Console.ReadKey();
+
+            //float altura, peso, imc;
+            //Console.WriteLine("qual sua altura: ");
+            //altura = float.Parse(Console.ReadLine());
+            //Console.WriteLine("qual o seu peso: ");
+            //peso = float.Parse(Console.ReadLine());
+            //imc = (altura + altura) / peso;
+            //Console.WriteLine("seu imc é " + imc);
+            //Console.WriteLine();
+
+            //ex 3 imc 
+
+            //float num1, num2, num3, peso1, peso2, peso3, media;
+
+            //Console.WriteLine("qual o primeiro número: ");
+            //num1 = float.Parse(Console.ReadLine());
+            //Console.WriteLine("qual o segundo número; ");
+            //num2 = float.Parse(Console.ReadLine());
+            //Console.WriteLine("qual o terceiro número: ");
+            //num3 = float.Parse(Console.ReadLine());
+            //Console.WriteLine("qual o primeiro peso: ");
+            //peso1 = float.Parse(Console.ReadLine());
+            //Console.WriteLine("qual o segundo peso: ");
+            //peso2 = float.Parse(Console.ReadLine());
+            //Console.WriteLine("qual o terceiro peso; ");
+            //peso3 = float.Parse(Console.ReadLine());
+
+            //media = (peso1 + peso2 + peso3) / (num1 + num2 + num3);
+            //Console.WriteLine("a media ponderada é de " + media);
+            //Console.ReadKey();
+            //ex 4 media ponderada 
+
+            //float acel, vel, temp;
+            //Console.WriteLine("qual é a velocidade: ");
+            //vel = float.Parse(Console.ReadLine());
+            //Console.WriteLine("qual a aceleração; ");
+            //acel = float.Parse(Console.ReadLine());
+            //Console.WriteLine("qual o tempo: ");
+            //temp = float.Parse(Console.ReadLine());
+
+            //acel = vel / temp;
+            //Console.WriteLine("a aceleração é " + acel + "metros por segundo");
+            //Console.ReadKey();
+            //ex 5 formula de fisica
+
+            //float sala, resul, porc;
+            //Console.WriteLine("qual o salario; ");
+            //sala = float.Parse(Console.ReadLine());
+
+            //porc = sala * 0.25f;
+            //resul = porc + sala;
+            //Console.WriteLine("o salario sera; "+ resul);
+            //Console.ReadKey():
+            //    ex 6 porcentagem
+
+            // flutua sal, re ,por ;
+            // Console.WriteLine("qual o valor do salario?");
+            // sal=float.Parse(Console.ReadLine());
+
+            // por = sal * 0.25f;
+            // re = por + sal;
+            // Console.WriteLine("o novo salário será de: " + re);
+
+            // Console.ReadKey();
+
+
+            // flutua re, dol;
+            // Console.WriteLine("quantos reais?");
+            // re=float.Parse(Console.ReadLine());
+
+            // dol = re * 5.20f;
+            // Console.WriteLine("o valor em dólares será de: "+dol);
+
+            // Console.ReadKey();
+
+
+            // int n, rn;
+
+            // Console.WriteLine("insira o numero: ");
+            // n = int.Parse(Console.ReadLine());
+            // rn = (n * 3 + 1) + (n * 2 - 1);
+
+            // Console.WriteLine("o valor é: " + rn);
+
+            // Console.ReadKey();
+
+
+
+            // int num;
+
+            // Console.Write("Digite um número inteiro de 4 dígitos: ");
+            // num = int.Parse(Console.ReadLine());
+
+            // Console.WriteLine(num / 1000);
+            // Console.WriteLine(num / 100 % 10);
+            // Console.WriteLine(num / 10 % 10);
+            // Console.WriteLine(num % 10);
+
+            // Console.ReadKey();
+
+
+            // saldo duplo = 1000,0;
+
+            // saldo = saldo * (1 - 0,034);
+            // saldo = saldo * (1 + 0,028);
+            // saldo = saldo * (1 + 0,14);
+            // saldo = saldo * (1 - 0,085);
+
+            // Console.WriteLine("O valor atual dos investimentos de Marquesito é: R$ " + saldo);
+
+            // Console.ReadKey();
+
+
+            // duplo num, qua, cub;
+
+            // Console.WriteLine("Digite o numero: ");
+            // num= double.Parse(Console.ReadLine());
+
+            // qua = num * num;
+            // cub=num*num*num;
+
+            // Console.WriteLine("o quadrado será de " + qua + " eo cubo será de " + cub);
+
+            // Console.ReadKey();
+
+
+            // flutua hom, mul, h;
+
+            // Console.WriteLine("Digite a altura: ");
+            // h=float.Parse(Console.ReadLine());
+
+            // hom = (72.7f * h) + 58.0f;
+            // mul = (62.1f * h) - 44.7f;
+
+            // Console.WriteLine("o peso ideal para mulher é: "+mul+" e o peso para homem é: "+hom);
+
+            // Console.ReadKey();
+
+
+            // GIT é um sistema de controle de versão de código aberto usado para rastrear alterações em arquivos e projetos colaborativos. O GitHub é uma plataforma de hospedagem de código-fonte que utiliza o GIT. Alguns comandos básicos do GIT incluem: git init, git add, git commit, git status, git log, git branch, git checkout, git merge, git pull e git push.
+
+        }
+    }
+}// flutua sal, re ,por ;
+            // Console.WriteLine("qual o valor do salario?");
+            // sal=float.Parse(Console.ReadLine());
+
+            // por = sal * 0.25f;
+            // re = por + sal;
+            // Console.WriteLine("o novo salário será de: " + re);
+
+            // Console.ReadKey();
+
+
+            // flutua re, dol;
+            // Console.WriteLine("quantos reais?");
+            // re=float.Parse(Console.ReadLine());
+
+            // dol = re * 5.20f;
+            // Console.WriteLine("o valor em dólares será de: "+dol);
+
+            // Console.ReadKey();
+
+
+            // int n, rn;
+
+            // Console.WriteLine("insira o numero: ");
+            // n = int.Parse(Console.ReadLine());
+            // rn = (n * 3 + 1) + (n * 2 - 1);
+
+            // Console.WriteLine("o valor é: " + rn);
+
+            // Console.ReadKey();
+
+
+
+            // int num;
+
+            // Console.Write("Digite um número inteiro de 4 dígitos: ");
+            // num = int.Parse(Console.ReadLine());
+
+            // Console.WriteLine(num / 1000);
+            // Console.WriteLine(num / 100 % 10);
+            // Console.WriteLine(num / 10 % 10);
+            // Console.WriteLine(num % 10);
+
+            // Console.ReadKey();
+
+
+            // saldo duplo = 1000,0;
+
+            // saldo = saldo * (1 - 0,034);
+            // saldo = saldo * (1 + 0,028);
+            // saldo = saldo * (1 + 0,14);
+            // saldo = saldo * (1 - 0,085);
+
+            // Console.WriteLine("O valor atual dos investimentos de Marquesito é: R$ " + saldo);
+
+            // Console.ReadKey();
+
+
+            // duplo num, qua, cub;
+
+            // Console.WriteLine("Digite o numero: ");
+            // num= double.Parse(Console.ReadLine());
+
+            // qua = num * num;
+            // cub=num*num*num;
+
+            // Console.WriteLine("o quadrado será de " + qua + " eo cubo será de " + cub);
+
+            // Console.ReadKey();
+
+
+            // flutua hom, mul, h;
+
+            // Console.WriteLine("Digite a altura: ");
+            // h=float.Parse(Console.ReadLine());
+
+            // hom = (72.7f * h) + 58.0f;
+            // mul = (62.1f * h) - 44.7f;
+
+            // Console.WriteLine("o peso ideal para mulher é: "+mul+" e o peso para homem é: "+hom);
+
+            // Console.ReadKey();
+
+
+            // GIT é um sistema de controle de versão de código aberto usado para rastrear alterações em arquivos e projetos colaborativos. O GitHub é uma plataforma de hospedagem de código-fonte que utiliza o GIT. Alguns comandos básicos do GIT incluem: git init, git add, git commit, git status, git log, git branch, git checkout, git merge, git pull e git push.
+
+             // flutua sal, re ,por ;
+            // Console.WriteLine("qual o valor do salario?");
+            // sal=float.Parse(Console.ReadLine());
+
+            // por = sal * 0.25f;
+            // re = por + sal;
+            // Console.WriteLine("o novo salário será de: " + re);
+
+            // Console.ReadKey();
+
+
+            // flutua re, dol;
+            // Console.WriteLine("quantos reais?");
+            // re=float.Parse(Console.ReadLine());
+
+            // dol = re * 5.20f;
+            // Console.WriteLine("o valor em dólares será de: "+dol);
+
+            // Console.ReadKey();
+
+
+            // int n, rn;
+
+            // Console.WriteLine("insira o numero: ");
+            // n = int.Parse(Console.ReadLine());
+            // rn = (n * 3 + 1) + (n * 2 - 1);
+
+            // Console.WriteLine("o valor é: " + rn);
+
+            // Console.ReadKey();
+
+
+
+            // int num;
+
+            // Console.Write("Digite um número inteiro de 4 dígitos: ");
+            // num = int.Parse(Console.ReadLine());
+
+            // Console.WriteLine(num / 1000);
+            // Console.WriteLine(num / 100 % 10);
+            // Console.WriteLine(num / 10 % 10);
+            // Console.WriteLine(num % 10);
+
+            // Console.ReadKey();
+
+
+            // saldo duplo = 1000,0;
+
+            // saldo = saldo * (1 - 0,034);
+            // saldo = saldo * (1 + 0,028);
+            // saldo = saldo * (1 + 0,14);
+            // saldo = saldo * (1 - 0,085);
+
+            // Console.WriteLine("O valor atual dos investimentos de Marquesito é: R$ " + saldo);
+
+            // Console.ReadKey();
+
+
+            // duplo num, qua, cub;
+
+            // Console.WriteLine("Digite o numero: ");
+            // num= double.Parse(Console.ReadLine());
+
+            // qua = num * num;
+            // cub=num*num*num;
+
+            // Console.WriteLine("o quadrado será de " + qua + " eo cubo será de " + cub);
+
+            // Console.ReadKey();
+
+
+            // flutua hom, mul, h;
+
+            // Console.WriteLine("Digite a altura: ");
+            // h=float.Parse(Console.ReadLine());
+
+            // hom = (72.7f * h) + 58.0f;
+            // mul = (62.1f * h) - 44.7f;
+
+            // Console.WriteLine("o peso ideal para mulher é: "+mul+" e o peso para homem é: "+hom);
+
+            // Console.ReadKey();
+
+
+            // GIT é um sistema de controle de versão de código aberto usado para rastrear alterações em arquivos e projetos colaborativos. O GitHub é uma plataforma de hospedagem de código-fonte que utiliza o GIT. Alguns comandos básicos do GIT incluem: git init, git add, git commit, git status, git log, git branch, git checkout, git merge, git pull e git push.
+            // flutua sal, re ,por ;
+            // Console.WriteLine("qual o valor do salario?");
+            // sal=float.Parse(Console.ReadLine());
+
+            // por = sal * 0.25f;
+            // re = por + sal;
+            // Console.WriteLine("o novo salário será de: " + re);
+
+            // Console.ReadKey();
+
+
+            // flutua re, dol;
+            // Console.WriteLine("quantos reais?");
+            // re=float.Parse(Console.ReadLine());
+
+            // dol = re * 5.20f;
+            // Console.WriteLine("o valor em dólares será de: "+dol);
+
+            // Console.ReadKey();
+
+
+            // int n, rn;
+
+            // Console.WriteLine("insira o numero: ");
+            // n = int.Parse(Console.ReadLine());
+            // rn = (n * 3 + 1) + (n * 2 - 1);
+
+            // Console.WriteLine("o valor é: " + rn);
+
+            // Console.ReadKey();
+
+
+
+            // int num;
+
+            // Console.Write("Digite um número inteiro de 4 dígitos: ");
+            // num = int.Parse(Console.ReadLine());
+
+            // Console.WriteLine(num / 1000);
+            // Console.WriteLine(num / 100 % 10);
+            // Console.WriteLine(num / 10 % 10);
+            // Console.WriteLine(num % 10);
+
+            // Console.ReadKey();
+
+
+            // saldo duplo = 1000,0;
+
+            // saldo = saldo * (1 - 0,034);
+            // saldo = saldo * (1 + 0,028);
+            // saldo = saldo * (1 + 0,14);
+            // saldo = saldo * (1 - 0,085);
+
+            // Console.WriteLine("O valor atual dos investimentos de Marquesito é: R$ " + saldo);
+
+            // Console.ReadKey();
+
+
+            // duplo num, qua, cub;
+
+            // Console.WriteLine("Digite o numero: ");
+            // num= double.Parse(Console.ReadLine());
+
+            // qua = num * num;
+            // cub=num*num*num;
+
+            // Console.WriteLine("o quadrado será de " + qua + " eo cubo será de " + cub);
+
+            // Console.ReadKey();
+
+
+            // flutua hom, mul, h;
+
+            // Console.WriteLine("Digite a altura: ");
+            // h=float.Parse(Console.ReadLine());
+
+            // hom = (72.7f * h) + 58.0f;
+            // mul = (62.1f * h) - 44.7f;
+
+            // Console.WriteLine("o peso ideal para mulher é: "+mul+" e o peso para homem é: "+hom);
+
+            // Console.ReadKey();
+
+
+            // GIT é um sistema de controle de versão de código aberto usado para rastrear alterações em arquivos e projetos colaborativos. O GitHub é uma plataforma de hospedagem de código-fonte que utiliza o GIT. Alguns comandos básicos do GIT incluem: git init, git add, git commit, git status, git log, git branch, git checkout, git merge, git pull e git push.
+
+     
+            // Console.WriteLine("qual o valor do salario?");
+            // sal=float.Parse(Console.ReadLine());
+
+            // por = sal * 0.25f;
+            // re = por + sal;
+            // Console.WriteLine("o novo salário será de: " + re);
+
+            // Console.ReadKey();
+
+
+            // flutua re, dol;
+            // Console.WriteLine("quantos reais?");
+            // re=float.Parse(Console.ReadLine());
+
+            // dol = re * 5.20f;
+            // Console.WriteLine("o valor em dólares será de: "+dol);
+
+            // Console.ReadKey();
+
+
+            // int n, rn;
+
+            // Console.WriteLine("insira o numero: ");
+            // n = int.Parse(Console.ReadLine());
+            // rn = (n * 3 + 1) + (n * 2 - 1);
+
+            // Console.WriteLine("o valor é: " + rn);
+
+            // Console.ReadKey();
+
+
+
+            // int num;
+
+            // Console.Write("Digite um número inteiro de 4 dígitos: ");
+            // num = int.Parse(Console.ReadLine());
+
+            // Console.WriteLine(num / 1000);
+            // Console.WriteLine(num / 100 % 10);
+            // Console.WriteLine(num / 10 % 10);
+            // Console.WriteLine(num % 10);
+
+            // Console.ReadKey();
+
+
+            // saldo duplo = 1000,0;
+
+            // saldo = saldo * (1 - 0,034);
+            // saldo = saldo * (1 + 0,028);
+            // saldo = saldo * (1 + 0,14);
+            // saldo = saldo * (1 - 0,085);
+
+            // Console.WriteLine("O valor atual dos investimentos de Marquesito é: R$ " + saldo);
+
+            // Console.ReadKey();
+
+
+            // duplo num, qua, cub;
+
+            // Console.WriteLine("Digite o numero: ");
+            // num= double.Parse(Console.ReadLine());
+
+            // qua = num * num;
+            // cub=num*num*num;
+
+            // Console.WriteLine("o quadrado será de " + qua + " eo cubo será de " + cub);
+
+            // Console.ReadKey();
+
+
+            // flutua hom, mul, h;
+
+            // Console.WriteLine("Digite a altura: ");
+            // h=float.Parse(Console.ReadLine());
+
+            // hom = (72.7f * h) + 58.0f;
+            // mul = (62.1f * h) - 44.7f;
+
+            // Console.WriteLine("o peso ideal para mulher é: "+mul+" e o peso para homem é: "+hom);
+
+            // Console.ReadKey();
+
+
+           
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
